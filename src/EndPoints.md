2e2cfd2d210b4609f85b3b505dc64664d9b25c16
clasesucatmarlon <clasesucatmarlon@gmail.com>
2024-03-04 18:07:27 -0500

    feat(Mern-crud-auth): :sparkles: Created login funcionality
    Created login funcionality

--- src/EndPoints.md
+++ src/EndPoints.md
@@ -18,4 +18,23 @@ http://localhost/api/register
           "password": "marlon12345",
           "username": "clasesucatmarlon4"
         }
+```  
+  
+
+## Login:
+```
+http://localhost/api/register
+
+    body:
+        {
+          "email": "marlon",
+          "password": "password",
+          "username": "username"
+        }
+
+        {
+          "email": "clasesucatmarlon4@gmail.com",
+          "password": "marlon12345",
+          "username": "clasesucatmarlon4"
+        }
 ```
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,10 +3,21 @@ import bcrypt from "bcryptjs";
 import { createAccessToken } from "../libs/jwt.js";
 
 
+/**
+ * The function `register` handles user registration by hashing the password, saving the user data to
+ * the database, generating an access token, setting a cookie, and returning user information in JSON
+ * format.
+ * @param req - The `req` parameter in the `register` function is an object representing the HTTP
+ * request. It contains information about the request made to the server, such as the request headers,
+ * body, parameters, and more. In this specific function, `req.body` is used to extract the `email`,
+ * @param res - The `res` parameter in the `register` function is the response object that is used to
+ * send a response back to the client making the request. It is typically used to send data, set
+ * cookies, and provide status codes in response to the client's request.
+ */
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
   try {
-    const pswHash = await bcrypt.hash(password, 10)
+    const pswHash = await bcrypt.hash(password, 10);
     const newUser = new User({
       email,
       username,
@@ -28,17 +39,46 @@ export const register = async (req, res) => {
       updatedAT: userSaved.updatedAt
     });
   } catch (error) {
-    res.status(500).json({message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
 
-// {
-//   "email": "clasesucatmarlon5@gmail.com",
-//   "password": "marlon12345",
-//   "username": "clasesucatmarlon5"
-// }
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const userFound = await User.findOne({ email });
+    if (!userFound) {
+      res.status(400).json({ message: "User not found" });
+    }
 
-export const login = (req, res) => {
-  res.send("Login....");
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch) {
+      res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // GENERAR TOKEN
+    const token = await createAccessToken({ id: userFound._id });
+
+    // GENERAR COOKIE
+    res.cookie("token", token);
+
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAT: userFound.createdAt,
+      updatedAT: userFound.updatedAt
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+export const logout = (req, res) => {
+  res.cookie("token", "", {
+    expires: new Date(0)
+  });
+  return res.sendStatus(200);
 };
